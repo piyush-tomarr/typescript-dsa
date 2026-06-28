497d880921263e301ae2e0df4702249d7fd50431
Piyush Tomar <itspiyush194@gmail.com>
2026-06-28 21:51:09 +0530

    Time: 1 ms (45.9%), Space: 56.2 MB (13.07%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.ts
@@ -0,0 +1,23 @@
+
+function sortColors(nums: number[]): void {
+let low:number = 0
+let mid:number =0
+let high:number = nums.length-1
+
+while(mid<=high){
+  if(nums[mid] === 0){
+    [nums[low] ,nums[mid]] = [nums[mid],nums[low]]
+
+    low++;
+    mid++
+  }
+
+  else if(nums[mid]===1){
+    mid++
+  }
+  else{
+    [nums[mid],nums[high]] =[nums[high],nums[mid]]
+    high --
+  }
+}
+};
