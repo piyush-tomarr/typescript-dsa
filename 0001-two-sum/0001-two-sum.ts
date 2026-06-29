9f29c031525a22d26d2f5380aeb46315d0332421
Piyush Tomar <itspiyush194@gmail.com>
2026-06-29 23:01:50 +0530

    Time: 2 ms (72.42%), Space: 57.6 MB (26.68%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.ts
@@ -0,0 +1,13 @@
+let twoSum = (nums:number[] , target:number):number[]=>{
+
+let map = new Map<number,number>()
+
+for(let i = 0 ; i<nums.length ; i++){
+    let complement = target - nums[i]
+    if(map.has(complement)){
+        return ([map.get(complement) , i])
+    }
+    map.set(nums[i] ,i )
+}
+ return []
+}
