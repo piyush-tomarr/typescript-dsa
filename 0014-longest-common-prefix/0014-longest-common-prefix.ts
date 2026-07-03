ce62b7febdaa67b96241d09cc4a962f981d6c638
Piyush Tomar <itspiyush194@gmail.com>
2026-07-03 23:34:40 +0530

    Time: 0 ms (100%), Space: 55.3 MB (83.75%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.ts
@@ -0,0 +1,12 @@
+ let strs = ["flower","flow","flight"]
+function longestCommonPrefix(strs: string[]): string { 
+let prefix = strs[0]
+
+for(let  i = 0 ; i <strs.length ;i++){
+    while(!strs[i].startsWith(prefix)){
+        prefix = prefix.slice(0,-1)
+    }
+}
+return prefix
+
+}
