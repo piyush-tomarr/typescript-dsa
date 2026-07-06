0988d4a6d06b0db21029493caf5dd2fcc867ac45
Piyush Tomar <itspiyush194@gmail.com>
2026-07-06 22:43:38 +0530

    Time: 0 ms (100%), Space: 61.9 MB (69.74%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.ts
@@ -0,0 +1,13 @@
+function reverseString(s: string[]): void {
+     let left = 0
+    let temp:string
+    let right = s.length-1
+
+    while(left<right){
+        temp=s[left]
+        s[left]=s[right]
+        s[right]=temp
+        left++
+        right--
+    }
+};
